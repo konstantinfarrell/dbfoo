e67805bf08125cb54034b2dface0d6c3d885fbd4
Konstantin Farrell <konstantinfarrell@gmail.com>
2016-07-06 10:44:58 -0700

    Add more user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -45,12 +45,48 @@ class TestUser(TestCase):
         """
         with open('dbfoo/data/states.txt', 'r') as states:
             states = states.read().splitlines()
-            u = User()
-            state = u.random_state()
-            new_state = u.random_state()
+            state = self.users.random_state()
+            new_state = self.users.random_state()
             self.assertIn(state, states)
             self.assertIn(new_state, states)
-            self.assertFalse(new_state == state)
+            self.assertNotEqual(new_state, state)
+
+    def test_random_phone(self):
+        """
+        Tests that a random phone number is the correct length
+        and that two random phone numbers are unique.
+        """
+        phone = self.users.random_phone()
+        other_phone = self.users.random_phone()
+        self.assertEqual(len(phone), 10)
+        self.assertNotEqual(phone, other_phone)
+
+    def test_random_city(self):
+        """
+        Tests that a random city is chosen from the list
+        of cities, and that two random cities are unique.
+        """
+        with open('dbfoo/data/cities.txt', 'r') as cities:
+            cities = cities.read().splitlines()
+            city = self.users.random_city()
+            other_city = self.users.random_city()
+            self.assertIn(city, cities)
+            self.assertIn(other_city, cities)
+            self.assertNotEqual(city, other_city)
+
+    def test_create_email(self):
+        """
+        Generates a first and last name for a user, then
+        tests to ensure the first and last name are contained
+        within the generated email address.
+        """
+        self.users.first_name = self.users.random_first_name()
+        self.users.last_name = self.users.random_last_name()
+        username = "{}{}".format(self.users.first_name, self.users.last_name)
+        email = self.users.create_email(username)
+        self.assertIn("{}{}{}".format(self.users.first_name,
+                                      self.users.last_name,
+                                      '@'), email)
 
 
 if __name__ == "__main__":
